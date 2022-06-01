0ee7d9698e7961783d208a8963493e01c8081516
arifvtti <arif.vtti@gmail.com>
2022-06-01 14:52:54 +0600

    make an api that receive data from client side

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const res = require("express/lib/response");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hpm3z.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const collection = client.db("allStock").collection("stock");
+
+        app.post("/stock", async (req, res) => {
+            const doc = req.body;
+            const result = await collection.insertOne(doc);
+            res.send(result);
+        })
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get("/", (req, res) => {
     res.send("Server for quikie app!")
